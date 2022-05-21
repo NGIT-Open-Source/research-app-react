aa0edd12773fb21139fd9f9e63c78f9956e7fbe9
jayendra_madara <dimebeatengreen8@gmail.com>
2022-05-21 17:31:12 +0530

    deployment

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,14 +1,18 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, Store } from 'redux'
 import { HYDRATE, createWrapper } from 'next-redux-wrapper'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import login from './Login/reducer'
+import { STATE } from '../typings'
+import { configureStore } from '@reduxjs/toolkit'
 
 const combinedReducer = combineReducers({
   login,
 })
 
 const initStore = () => {
-  return createStore(combinedReducer, composeWithDevTools(applyMiddleware()))
+  return configureStore({
+    reducer: { login: login },
+  })
 }
 
-export const wrapper = createWrapper(initStore)
+export const wrapper = createWrapper<Store<STATE>>(initStore)
--- typings.d.ts
+++ typings.d.ts
@@ -6,5 +6,4 @@ export interface STATE {
 
 interface ACTION {
   type: string
-  payload: any
 }
